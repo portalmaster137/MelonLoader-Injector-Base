1e6ba681042a89ab0848b48174fb30dc321385a9
Portalmaster137 <portalmaster137@gmail.com>
2022-11-18 19:10:02 +1000

    loader

---
+++ Mod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(GeneralModLoader.Loader), "Loader", "1.0.0", "Porta")]
+
+namespace GeneralModLoader
+{
+    public class Loader { }
+}
